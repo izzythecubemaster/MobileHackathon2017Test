2475ec6d488c141be67019c404ad028776328df2
RURALSOURCE\israel.montoya <israel.montoya@ruralsourcing.com>
2017-03-22 09:17:35 -0600

    Added VisitorLog model and SQLite to project

---
+++ MobileHackathon2017/Models/VisitorLog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SQLite;
+using SQLitePCL;
+
+namespace MobileHackathon2017.Models
+{
+    public class VisitorLog
+    {
+        [PrimaryKey]
+        public int DLID { get; set; }
+        [MaxLength(9)]
+        public DateTime TimeIn { get; set; }
+        public String Name { get; set; }
+        public String TimeOut { get; set; }
+    }
+}
