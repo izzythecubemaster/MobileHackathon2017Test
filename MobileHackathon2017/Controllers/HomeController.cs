d94a59545f242312d10836bcebec963199c2967c
RURALSOURCE\israel.montoya <israel.montoya@ruralsourcing.com>
2017-03-22 09:41:32 -0600

    Added AddVisitor method to controller, and index page

--- MobileHackathon2017/Controllers/HomeController.cs
+++ MobileHackathon2017/Controllers/HomeController.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SQLite;
+using SQLitePCL;
+using MobileHackathon2017.Models;
 
 namespace MobileHackathon2017.Controllers
 {
     public class HomeController : Controller
     {
+        public HomeController()
+        {
+            var db = new SQLiteConnection("VLog");
+            db.CreateTable<VisitorLog>();
+        }
         public ActionResult Index()
         {
+            
             return View();
         }
 
@@ -25,6 +34,16 @@ namespace MobileHackathon2017.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public static void AddVisitor(SQLiteConnection db, int DL, string n, string TO)
+        {
+            var s = db.Insert(new VisitorLog()
+            {
+                DLID = 0,
+                TimeIn = DateTime.Now,
+                Name = n,
+                TimeOut = TO
+            });
         }
     }
 }
